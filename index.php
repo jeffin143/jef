7e1c3a8d17406fe30c42510655542b85d4d6940d
jeffin sam <jeffinsam@karunya.edu.in>
2017-03-12 22:11:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="bookpagestyle.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+       
+        <!-- header files- navigation,search,logo -->
+        
+        <header class="head">
+            <div id="login_opt">
+                <a href="#">Login</a>
+            </div>
+            <div class="clear">
+                
+            </div>
+            
+            <div id="header">
+                
+            <div class="search">
+                <form>
+                    <input type="search" id="search" name="search" placeholder="search here..."/>
+                    <input type="submit" id="search_btn" name="search_btn"value="search"
+                </form>
+            </div>
+            <div class="clear">
+                
+            </div>
+            <div class="menu_bar">
+                <ul>
+                   
+                    <li><a href="#">Category</a></li>
+                    
+                    <li><a href="#">ggh</a></li>
+                    <li><a href="#">frfrf</a></li>
+                    <li><a href="#">rfrfrf</a></li>
+                </ul>
+            </div>
+            
+            </div>
+        </header>
+       
+        
+        <!-- mid-image slide,-->
+        
+        
+        
+        <div class="mid">
+            <img  id="img1"  src="background.jpg"/>
+        </div>
+       
+        
+        
+        <!-- main body-content -->
+        
+        
+        
+        <section class="main">
+            fgfg
+        </section>
+        
+    </body>
+</html>
